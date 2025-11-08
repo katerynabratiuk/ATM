b8b46cc3e58038614195077ec5c74d2e7724d35a
Vlad Panko <vvvladpanko@gmail.com>
2025-11-08 22:37:53 +0200

    feat: change balance page logic slightly

--- frontend/BalanceWidget.cpp
+++ frontend/BalanceWidget.cpp
@@ -23,21 +23,17 @@ void BalanceWidget::show()
 {
 	try
 	{
-		_ui.clTitle->setVisible(false);
-		_ui.clValue->setVisible(false);
-
 		Card card/* = _cardController.getCard()*/;
 		_ui.bValue->setText(QString::number(card._balance));
-		if (card._creditLimit > 0)
+		if (card._creditLimit <= 0)
 		{
-			_ui.clTitle->setVisible(true);
-			_ui.clValue->setVisible(true);
-			_ui.clValue->setText(QString::number(card._creditLimit));
+			_ui.clValue->setText("not a credit card");
 		}
 	}
 	catch (const std::exception& e)
 	{
-		_ui.bValue->setText("Error!");
+		_ui.bValue->setText("error");
+		_ui.clValue->setText("error");
 	}
 }
 
