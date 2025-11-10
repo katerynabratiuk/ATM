89c634ef6f6df77e57711abb2fa42914d696a37b
Vlad Panko <vvvladpanko@gmail.com>
2025-11-10 03:12:47 +0200

    feat: start implementing banknotes logic

--- backend/services/BanknoteService.cpp
+++ backend/services/BanknoteService.cpp
@@ -1,8 +1,18 @@
 #include "backend/services/BanknoteService.h"
+#include <unordered_map>
 
 BanknoteService::BanknoteService(IBanknoteRepository& repo) : _repo(repo) {}
 
 void BanknoteService::doDispense(int amount)
 {
-    return;
+	try
+	{
+		std::unordered_map<Denominations, int> counts = _repo.getAllCounts();
+	}
+	catch (const std::exception& e)
+	{
+		throw;
+	}
+
+
 }
