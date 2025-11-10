3638d1ef828178d6ef7eb7c0aad1f744bfd653c6
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-11-10 06:05:46 +0200

    Add Banknote repository

--- backend/repositories/IBanknoteRepository.h
+++ backend/repositories/IBanknoteRepository.h
@@ -5,24 +5,21 @@
 class IBanknoteRepository
 {
 public:
-	virtual ~IBanknoteRepository() = default;
+    virtual ~IBanknoteRepository() = default;
 
-	void getCount(Denominations denom)
-	{
-		return doGetCount(denom);
-	}
+    int getCount(Denominations denom) { return doGetCount(denom); }
+    void setCount(Denominations denom, int count) { doSetCount(denom, count); }
+    std::vector<std::pair<Denominations, int>> getAllCounts() { return doGetAllCounts(); }
 
-	void setCount(Denominations denom, int count)
-	{
-		return doSetCount(denom, count);
-	}
+protected:
+    explicit IBanknoteRepository(long atmId)
+        : _atmId(atmId) {
+    }
 
-	std::vector<std::pair<Denominations, int>> getAllCounts()
-	{
-		return doGetAllCounts();
-	}
+    const long _atmId;
+
 private:
-	virtual void doGetCount(Denominations denom) = 0;
-	virtual void doSetCount(Denominations denom, int count) = 0;
-	virtual std::vector<std::pair<Denominations, int>> doGetAllCounts() = 0;
+    virtual int doGetCount(Denominations denom) = 0;
+    virtual void doSetCount(Denominations denom, int count) = 0;
+    virtual std::vector<std::pair<Denominations, int>> doGetAllCounts() = 0;
 };
