b6a662dbb009d5666a0c57a5a99a476ccc2564ab
Данило Майстер <d.maister@ukma.edu.ua>
2025-11-15 22:28:35 +0200

    fied tests

--- tests/mocks/MockBanknoteService.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "backend/services/IBanknoteService.h"
-#include "gmock/gmock.h"
-
-class MockBanknoteService : public IBanknoteService {
-public:
-    MOCK_METHOD(void, doDispense, (int amount), (override));
-};
