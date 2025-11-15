48fa676229798bd0bd48a52573d7be6625baa7ce
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-11-15 03:52:38 +0200

    add library for reading .env + configure CMake for vs

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include "frontend/ATMInterface.h"
 #include <QtWidgets/QApplication>
+#include <filesystem>
+#include <iostream>
+#include "backend/external/dotenv.h"
 
 int main(int argc, char *argv[])
 {
+    dotenv::init("../../.env");
+
+    if (const char* db_name = std::getenv("DB_NAME")) {
+        std::cout << "DB_NAME=" << db_name << std::endl;
+    }
+    else {
+        std::cerr << "DB_NAME is not set!" << std::endl;
+    }
+
     QApplication app(argc, argv);
     ATMInterface window;
     window.show();
